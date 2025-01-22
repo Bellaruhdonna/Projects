0d46bd4e092f8baded2b42dbe59bcec0608a634b
Purvasha Mathur <forpurvashamathur@gmail.com>
2025-01-22 17:03:07 +0530

    Delete mini projects directory

--- mini projects/ani.py
+++
@@ -1,4 +0,0 @@
-c = int (input("enter temp in celcius: "))
-f = 9*c/5 + 32
-print (c,  f)
-#print(f"{c}celcius={f}farheniet")
--- mini projects/dice.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def roll_dice():
-    d_result= random.randint(1,6)
-    print(d_result)
-
-roll_dice()
